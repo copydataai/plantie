7e6349272a18ae9a1d6bc430817e210a71510683
Jose Sanchez <engineer@copydataai.com>
2025-03-26 14:06:38 +0000

    fix(config): update next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [{
+      protocol: "https",
+      hostname: "gi1n4aj2u4.ufs.sh",
+    }]
+  }
 };
 
 export default nextConfig;
